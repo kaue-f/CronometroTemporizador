90153bd339b8e633ff15205891d6f3fac28e4ef0
Kaue Francisco <kaue_francisco70@hotmail.com>
2023-08-05 22:28:17 -0300

    Temporizador

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Cronometro;
+﻿namespace CronometroTemporizador;
 
 class Program
 {
@@ -7,65 +7,23 @@ class Program
         Menu();
     }
 
-    static void Menu()
+    public static void Menu()
     {
         Console.Clear();
-        Console.WriteLine("S = Segundo => 10s = 10 segundos");
-        Console.WriteLine("M = Minuto => 1m = 1 minuto");
-        Console.WriteLine("H = Hora => 1h = 1 hora");
-        Console.WriteLine("0 = Sair");
+        Console.WriteLine("1 - Cronômetro");
+        Console.WriteLine("2 - Temporizador");
+        Console.WriteLine("0 - Sair");
         Console.WriteLine("----------------------");
-        Console.WriteLine("Quanto tempo deseja contar?");
+        Console.WriteLine("Selecione uma opção");
 
-        string data = Console.ReadLine().ToLower();
-        char type = char.Parse(data.Substring(data.Length - 1, 1)); //Pegar o ultimo digito
-        int time = int.Parse(data.Substring(0, data.Length - 1)); //Pegar os digito do time
+        var opcao = Console.ReadLine().ToLower();
 
-        int multiplicador = 1;
-
-        // Multiplica m por s
-        if (type == 'm')
-            multiplicador = 60;
-
-        // Multiplica h por s
-        if (type == 'h')
-            multiplicador = 3600;
-
-        if (time == 0)
-            System.Environment.Exit(0);
-
-        PreStart(time * multiplicador);
-    }
-
-    static void PreStart(int time)
-    {
-        Console.Clear();
-        Console.WriteLine("Ready...");
-        Thread.Sleep(1000);
-        Console.WriteLine("Set...");
-        Thread.Sleep(1000);
-        Console.WriteLine("Go...");
-        Thread.Sleep(2500);
-
-        Start(time);
-    }
-
-    static void Start(int time)
-    {
-        int currentTime = 0;
-
-        while (currentTime != time)
+        switch (opcao)
         {
-            Console.Clear();
-            currentTime++;
-            Console.WriteLine(currentTime);
-            Thread.Sleep(1000);
+            case "1": Cronometro.Menu(); break;
+            case "2": Temporizador.Menu(); break;
+            default: System.Environment.Exit(0); break;
         }
-
-        Console.Clear();
-        Console.WriteLine("Cronômetro Finalizado");
-        Thread.Sleep(5000);
-        Menu();
     }
 }
 
---
+++ Temporizador.cs
@@ -0,0 +1,69 @@
+namespace CronometroTemporizador
+{
+    public class Temporizador
+    {
+        public static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("H = Hora => 1h = 1 hora");
+            Console.WriteLine("0 = Voltar");
+
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+
+            if (data == "0")
+                data = "0s";
+
+            char type = char.Parse(data.Substring(data.Length - 1, 1)); //Pegar o ultimo digito
+            int time = int.Parse(data.Substring(0, data.Length - 1)); //Pegar os digito do time
+
+            int multiplicador;
+
+            switch (type)
+            {
+                case 's': multiplicador = 1; PreStart(time * multiplicador); break;
+                case 'm': multiplicador = 60; PreStart(time * multiplicador); break;
+                case 'h': multiplicador = 3600; PreStart(time * multiplicador); break;
+                default: Program.Menu(); break;
+            }
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (time != currentTime)
+            {
+                Console.Clear();
+                Console.WriteLine(time);
+                time--;
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine(0);
+            Thread.Sleep(1000);
+
+            Console.Clear();
+            Console.Beep();
+            Console.WriteLine("Temporizador Finalizado");
+            Thread.Sleep(3000);
+            Program.Menu();
+        }
+    }
+}
